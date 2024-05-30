d1a4a1de389c6b42f95a79af324d83daf139fa6a
Mateus Paula <mateusdesouzapaula97@gmail.com>
2024-05-30 14:14:36 -0300

    add: update 5 files and create 1 file - adding a skeleton component to the home page, as well as adding  setTimeout function to take time for the /products/featured route to be resolved so that we can see the animation

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,12 @@
+import { Skeleton } from "@/components/skeleton";
+
 export default function HomeLoading() {
-  return <p>Carregando...</p>;
+  return (
+    <div className="grid h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="h-full col-span-6 row-span-6 rounded-lg" />
+
+      <Skeleton className="h-full col-span-3 row-span-3 rounded-lg" />
+      <Skeleton className="h-full col-span-3 row-span-3 rounded-lg" />
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    gridTemplateRows: {
-      app: "min-content max-content",
+    extend: {
+      gridTemplateRows: {
+        app: "min-content max-content",
+      },
     },
   },
   plugins: [],
