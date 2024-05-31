c0549457bd6314c1005b6b0b129e2c269d997653
Mateus Paula <mateusdesouzapaula97@gmail.com>
2024-05-31 12:26:32 -0300

    add: update 3 files and create 1 file - adding the logic displaying the data filtered by the q parameter of searchParams, also adding a loading page for the /search page

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,20 @@
+import { Skeleton } from "@/components/skeleton";
+
 export default function SearchLoading() {
-  return <p>Carregando busca...</p>;
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">Carregando Resultados</p>
+
+      <div className="grid grid-cols-3 gap-6">
+        {Array.from({ length: 6 }).map((_, index) => {
+          return (
+            <Skeleton
+              key={index}
+              className="aspect-square relative rounded-lg"
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
